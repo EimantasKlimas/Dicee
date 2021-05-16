ce2a989165cfa4282fd0ee3a9af2026fe6ed30fd
Eimantas <eim.klimas@gmail.com>
2021-05-16 19:44:18 +0300

    Initial Commit

---
+++ dice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dice
+//
+//  Created by Eimantas Klimas on 2021-05-16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
